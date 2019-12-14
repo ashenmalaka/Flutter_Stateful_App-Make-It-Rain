3d8e83725a5fe6a5af13cf6f101794c1f35f959e
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-12-14 21:47:57 +0530

    Created appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './ui/make_it_rain.dart';
 
 void main(){
   runApp(new MaterialApp(
--- lib/ui/make_it_rain.dart
+++ lib/ui/make_it_rain.dart
@@ -1,2 +1,23 @@
 import 'package:flutter/material.dart';
 
+class MakeItRain extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    
+    return new MakeItRainState();
+    }
+}
+    
+class MakeItRainState extends State<MakeItRain> {
+  @override
+  Widget build(BuildContext context) {
+  
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text("Make It Rain!"),
+        backgroundColor: Colors.lightGreen,
+        ),
+    );
+  }
+
+}
